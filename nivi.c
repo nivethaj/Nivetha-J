409143b238be2a02d23feff451342c1104301768
nivethaj <30828724+nivethaj@users.noreply.github.com>
2017-09-01 11:47:25 +0530

    Create nivi.c

---
+++ nivi.c
@@ -0,0 +1,15 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+int n;
+printf("\n enter the number:");
+if(n>1)
+{
+printf("\n The number is positive");
+else
+{
+printf("\n the number is negative");
+}
+}
+}
