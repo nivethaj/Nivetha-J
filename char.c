3000d7ed1cda95d7144434a08dac611076057261
nivethaj <30828724+nivethaj@users.noreply.github.com>
2017-09-05 16:14:48 +0530

    Create char.c

---
+++ char.c
@@ -0,0 +1,14 @@
+void main()
+{
+char m;
+int a,b;
+printf("\n enter an character:");
+scanf(%s',&m);
+if(m==a||m==e||m==i||m==o||m==u);
+(
+printf( "\n This is vowel");
+else
+{
+printf("\n this is constant");
+}
+}
