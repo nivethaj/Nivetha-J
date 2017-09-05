74ae26e3d19bb75c37edfb8ab4d67dad3a92ce88
nivethaj <30828724+nivethaj@users.noreply.github.com>
2017-09-05 16:40:49 +0530

    Update constant.c

--- constant.c
+++ constant.c
@@ -1,17 +1,15 @@
 void main()
 {
-int n;
+char n;
 {
-switch(n)
-{
-case 1:
-{
-printf('\n given n is alphabet");
-}
-case 2:
-{
-printf("\n given n is not alphabet");
-]
-}
+printf("\n enter a character:");
+scanf("%S",&n");
+if(n==0)
+      {
+        printf("\n alphabet");
+        else
+          printf("\n not alphabet");
+        
+   }
 }
 }
