b0aab6e6bda8e0810edcf41335261f064fe0db5a
nivethaj <30828724+nivethaj@users.noreply.github.com>
2017-09-10 19:58:26 +0530

    Create base.c

---
+++ base.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+int base,exp;
+{
+printf("Enter a base number: ");
+scanf("%d", &base);
+}
+{
+printf("Enter an exp: ");
+scanf("%d", &exp);
+}
+{
+while(exp!=0)
+{
+result *= base;
+--exp;
+}
+return 0;
+}
