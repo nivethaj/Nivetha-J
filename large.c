8a92e83c19e7b842adb392f9210432a6152ff2a8
nivethaj <30828724+nivethaj@users.noreply.github.com>
2017-09-10 19:08:25 +0530

    Create new file

---
+++ large.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+int a,b,c;
+{
+printf("/n enter the number:");
+scanf("%d,%d,%d",&a,&b,&c);
+if(a>b)
+{
+printf("a is greater");
+}
+else
+{
+printf("b is greater");
+}
+if(b>c)
+{
+printf("b is greater");
+}
+else
+{
+printf("c is greater");
+}
+}
