dac8c703b1bff3a1723416b0941462624c5a7083
nivethaj <30828724+nivethaj@users.noreply.github.com>
2017-09-10 19:28:43 +0530

    Create sum.c

---
+++ sum.c
@@ -0,0 +1,20 @@
+ #include<stdio.h> 
+void main()
+{
+int n,i,sum=0;
+{    
+printf("Enter a number: ");
+scanf("%d",&n);
+}
+{
+for(i=1;i<=n;i++)
+{
+sum +=i;// sum = sum+i;
+}
+{
+printf("Sum = %d",sum);
+} 
+return 0;
+}
+
+
