d35957c4e7b96f3ae17eb34504741317b226376d
nivethaj <30828724+nivethaj@users.noreply.github.com>
2018-01-11 15:53:37 +0530

    Create arraymin.c

---
+++ arraymin.c
@@ -0,0 +1,22 @@
+#include<studio.h>
+void main();
+{
+int array[50],min,i,n;
+{
+printf("\n enter a array element : \n");
+scanf("%d",&array);
+{
+for(i=0;i>=n;i++)
+{
+scanf("%d",array[i]);
+}
+if(min=array[0])
+{
+array[i]--;
+}
+else
+{
+printf("\n print the min element:");
+}
+return 0;
+}
