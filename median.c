de4f4a32ea7458a86b7f161a054a8bbc295a1f37
nivethaj <30828724+nivethaj@users.noreply.github.com>
2018-01-11 16:24:32 +0530

    Create median.c

---
+++ median.c
@@ -0,0 +1,16 @@
+#includ<studio.h>
+void main()
+{
+int array[50],i,j,median;
+printf("\n Enter a array element: ");
+scanf("%d",&array");
+{
+for(i=0;i<=n-i;i++)
+for(j=0;j<=n+i;j++)
+{
+median=(n-1)/2-1;
+}
+printf("\n print the median element:");
+}
+return 0;
+}
