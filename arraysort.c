5e1fb9511022fd86547264c7b354a9ff67a98280
nivethaj <30828724+nivethaj@users.noreply.github.com>
2018-01-11 16:00:58 +0530

    Create arraysort.c

---
+++ arraysort.c
@@ -0,0 +1,13 @@
+#include,stdio.h>
+void main()
+{
+int n,i;
+printf("\n enter a sort array element:");
+scanf("%d,"&n);
+for(i=0;i<n;i++)
+{
+for(i=0;i<n-1;i==)
+printf("\n Sort the array element:");
+}
+return 0;
+}
