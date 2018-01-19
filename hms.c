30aef2f46f275254c07ad2efd9031ce99516f712
nivethaj <30828724+nivethaj@users.noreply.github.com>
2018-01-19 16:00:23 +0530

    Create hms.c

---
+++ hms.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+int time,hours,minute,sec;
+printf("\n Enter a minute:"):
+scanf('%d',&minute);
+{
+ hours = (sec/3600); 
+ minute=(sec-(3600*h))/60;
+}
+{
+printf("hour:minute:sec- %d:%d:%d \n",hour,minute,sec);
+}
+return 0;
+}
+
+	
+
