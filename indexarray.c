adebdef67e0ebb8104f21cd1ea33cb8872d7678f
nivethaj <30828724+nivethaj@users.noreply.github.com>
2018-01-19 15:53:11 +0530

    Create indexarray.c

---
+++ indexarray.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int[] Array = { 5, 8, 3, 7 };
+int Element = 3;
+int Index;
+{
+for (int i = 0; i < Array.Length; i++)
+{
+     if (Element == Array[i])
+     {
+         Index = i;
+     }
+    printf("\n the index element of an array:\n");
+ }
+   return 0;
+}}
