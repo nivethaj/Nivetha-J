dc02c807f9f917b13deb02d0e580bba2c4a98647
nivethaj <30828724+nivethaj@users.noreply.github.com>
2018-01-21 11:55:48 +0530

    Create arrayls.c

---
+++ arrayls.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void main()
+{
+int a[50],i,n,large,small;
+printf("How many elements:");
+scanf("%d",&n);
+printf("Enter the Array:");
+scanf("%d",&a[i]);
+{
+for(i=0;i<n;++i)
+large=small=a[0];
+}
+{
+for(i=1;i<n;++i)
+{
+if(a[i]>large)
+large=a[i];
+if(a[i]<small)
+small=a[i];
+}
+{
+printf("The largest element is %d",large);
+printf("\nThe smallest element is %d",small);
+}
+return 0;
+}
