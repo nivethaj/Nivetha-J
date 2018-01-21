5821799d3d79265f62f843fced83efc2a03b882f
nivethaj <30828724+nivethaj@users.noreply.github.com>
2018-01-21 11:59:35 +0530

    Create avgarr.c

---
+++ avgarr.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main() 
+{
+	int num,i,s=0,avg,a[100];
+	scanf("%d\n",&num);
+	for(i=0;i<num;i++)
+	{
+	scanf("%d ",&a[i]);
+	}
+	for(i=0;i<num;i++)
+	{
+	s=s+a[i];
+	}
+	avg=s/num;
+	printf("%d",avg);
+	return 0;
+}
