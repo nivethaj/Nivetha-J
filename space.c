71f6796a34e8ca3fc06a3501481c6efc795f8949
nivethaj <30828724+nivethaj@users.noreply.github.com>
2018-01-21 10:06:57 +0530

    Create space.c

---
+++ space.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int count=0;
+char word[200];
+printf("Enter a text:");
+gets[words];
+{
+if(words==0)
+space++;
+}
+printf("\n Number of White spaces: %d",spaces);
+return 0;
+}
