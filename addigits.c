869e8afb367da99fb276cff698f222ff7b1b2dc3
nivethaj <30828724+nivethaj@users.noreply.github.com>
2018-01-23 16:22:20 +0530

    Create addigits.c

---
+++ addigits.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+void main()
+{ 
+int n,temp,var,sum = 0;
+{
+printf("Enter the number: \n");
+scanf("%d",&num);
+temp = num;
+}
+{
+while(num>0)
+{
+var=num%10;
+sum=sum+var;
+num=10;
+}
+{
+printf("Given number = %d\n", temp);
+printf("Sum of the digits %d=%d\n", temp, sum);
+return 0;
+}
