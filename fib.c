0d3f93bdc1c5f6546a88903c6a9829758576639e
nivethaj <30828724+nivethaj@users.noreply.github.com>
2018-01-25 16:28:21 +0530

    Create fib.c

---
+++ fib.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int i,n,t1=0,t2=1,series;
+printf("Enter the number of series: ");
+scanf("%d", &n);
+printf("Fibonacci Series: ");
+for(i=1;i<=n;i++)
+ {
+ printf("%d",t1)
+ series=t1+t2);
+ t1=t2;
+ t2=series;
+ }
+   }
+    return 0;
+}
