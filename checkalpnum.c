bfb947ad5966942e0747774d26dbd111a7add28d
nivethaj <30828724+nivethaj@users.noreply.github.com>
2018-01-25 15:56:35 +0530

    Create checkalpnum.c

---
+++ checkalpnum.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int num;
+char k;
+printf("\n Enter a number and alphabet: ");
+scanf("%d %s",&num,&a);
+{
+if((k>='a'&& k<='z')||(k>='A'&& k<='Z')||(num>=0));
+{
+printf("\n yes");
+else
+printf("\n No");
+}
+return 0;
+}
+}
