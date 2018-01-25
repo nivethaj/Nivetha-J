ea586ecf1c36c23e53877a5f0491c18d7961e81e
nivethaj <30828724+nivethaj@users.noreply.github.com>
2018-01-25 16:01:00 +0530

    Create numswap.c

---
+++ numswap.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int a,b,temp; 
+printf("Enter the value of a and b: \n");
+scanf("%d%d",&a,&);
+printf("Before Swapping : ",a,b);
+{ 
+   temp=a;
+   a=b;
+   b=temp;
+}
+printf("After Swapping :",a,b);
+return 0;
+}
