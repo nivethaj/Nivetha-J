849aff5ea90983b5e4102801b4fcb951e9adb52b
nivethaj <30828724+nivethaj@users.noreply.github.com>
2018-02-01 10:30:56 +0000

    Create firstk.c

---
+++ firstk.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void  main()
+{
+  char a[50]={'success'};
+  int n,k;
+  printf("\n Enter k  number:");
+  scanf("%d",&k);
+  for(n=0;n<=k;n++)
+  printf("\n %c",a[n]);
+ }  
+  
