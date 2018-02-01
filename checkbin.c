26bf498e1d124f5b0ef777507fa241e5eeff586b
nivethaj <30828724+nivethaj@users.noreply.github.com>
2018-02-01 10:46:16 +0000

    Create checkbin.c

---
+++ checkbin.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void  main()
+{
+  int n[50],i,k;
+  printf("\n Binary number:");
+  scanf("%d",&n[i]);
+  if(i==0||i==1)
+  {
+      printf("\n It is Binary number");
+   }
+   else
+   {
+       printf("\n It is not binary number");
+  }
+} 
+  
