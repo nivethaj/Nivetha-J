26777f4ffbe3c76b649faeca78269d4d5b971caa
nivethaj <30828724+nivethaj@users.noreply.github.com>
2018-02-01 11:00:55 +0000

    Create wordcount.c

---
+++ wordcount.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void  main()
+{
+  char a[50];
+  int count,i;
+  printf("\n Enter a words:");
+  scanf("%s",&a[i]);
+  for(i=0;a[i]!=0;i++)
+  {
+       if(a[i]=="")
+       count++;
+  }
+  printf("Number of words : %d",count+1);
+}
+
