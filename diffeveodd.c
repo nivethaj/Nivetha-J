1b5f85a6b06e47a024f32b89ff719305f486cb81
nivethaj <30828724+nivethaj@users.noreply.github.com>
2018-02-02 14:40:46 +0530

    Create diffeveodd.c

---
+++ diffeveodd.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+   int a,b,diff;
+   printf("\n Enter a number:");
+   scanf("%d%d",&a,&b);
+   diff=a-b;
+   if(diff%2==0)
+    {
+       printf("\n even");
+    }
+    else
+    {
+       printf("\n odd");
+    } 
+    
+}   
+   
+   
