fa481e84d1c1f690549112f7959dd99d3944a8dc
nivethaj <30828724+nivethaj@users.noreply.github.com>
2018-02-06 09:00:00 +0000

    Create norange.c

---
+++ norange.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{ 
+  int a,b,c;
+  printf("\n Enter a limit:");
+  scanf("%d %d",&a,&b);
+  printf("enter the number:");
+  scanf("%d",&c); 
+  if(c>a && c<b)
+   {
+     printf("yes");
+    }
+  else
+  {
+    printf("No");
+   }
+}
