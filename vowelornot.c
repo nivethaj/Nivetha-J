338c942caeea3eba233b443bdd652380974b9274
nivethaj <30828724+nivethaj@users.noreply.github.com>
2018-02-06 08:46:22 +0000

    Create vowelornot.c

---
+++ vowelornot.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    char s;
+    printf("\n Enter a vowel:");
+    scanf("%s",&s);
+    if(s=='a'||s=='e'||s=='i'||s=='o'||s=='u')
+    {
+        printf("\n It is vowel");
+    }
+    else
+    {
+        printf("\n It is not vowel");
+    }
+}    
+    
