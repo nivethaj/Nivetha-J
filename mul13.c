8d4084e014789e96cff11db1ce3c3778079b0337
nivethaj <30828724+nivethaj@users.noreply.github.com>
2018-02-08 06:20:52 -0300

    Create mul13.c

---
+++ mul13.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+  int n,mul;
+  printf("\n Enter a number:");
+  scanf("%d",&n);
+  mul=n*13;
+  if(mul==0)
+  {
+  printf("\n yes");
+  }
+  else
+  {
+      printf("\n no");
+  }
+}  
