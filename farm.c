3a6082ac1b3815c8d61db7f412a7ddbd07270d3e
nivethaj <30828724+nivethaj@users.noreply.github.com>
2018-02-09 09:32:01 +0000

    Create farm.c

---
+++ farm.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+   float len,br,area;
+   printf("\n Enter a length:");
+   scanf("%f",&len);
+   printf("\n Enter a breadth:");
+   scanf("%f",&br);
+   area=len*br;
+   printf("\n The area of the farm is: %f",area);
+}
+   
