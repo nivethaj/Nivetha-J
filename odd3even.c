96ba9058b8b81ca58fec17e3fcb1f741920b7635
nivethaj <30828724+nivethaj@users.noreply.github.com>
2018-02-13 08:54:27 +0000

    Create odd3even.c

---
+++ odd3even.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include<string.h>
+void main()
+{
+	char s[10];
+	int i,l;
+	scanf("%s",s);
+	l=strlen(s);
+	for(i=0;i<l;i++)
+	{
+    if(i%2==0)
+	printf("%c",s[i]);
+	}
+   for(i=0;i<l;i++)
+	{
+   if(i%2!=0)
+  printf("%c",s[i]);
+}
+   return 0;
+}
+
+ 
+
