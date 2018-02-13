ba7918eefc2bbc90d69b1eff10ddfbbfd1a281e7
nivethaj <30828724+nivethaj@users.noreply.github.com>
2018-02-13 08:41:43 +0000

    Create asciival.c

---
+++ asciival.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() 
+{	
+   char a;
+   int n;
+   printf("\n Enter a Character:");
+   scanf("%s",&a);
+   {
+      n=(int)a;
+      printf("\n ASCII Value is %d",n);
+   }
+   return 0;
+}
+
+ 
+
