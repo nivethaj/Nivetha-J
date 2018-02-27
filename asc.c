9d00077f854d9b723752090243f6b0b6404ca3f8
nivethaj <30828724+nivethaj@users.noreply.github.com>
2018-02-27 16:19:07 +0530

    Create asc.c

---
+++ asc.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main()
+{
+  int n,i,a[10];
+  scanf("%d",&n);
+  for(i=1;i<=n;i++)
+  {
+      scanf("%d",&a[i]);
+      if(a[i]!=i)
+      {
+          printf("%d",i);
+      }
+  }
+   return 0;
+}
