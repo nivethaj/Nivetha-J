0f93d2fb8fed5e41b0c7db4e70f7a7e125673752
nivethaj <30828724+nivethaj@users.noreply.github.com>
2018-03-01 15:54:53 +0530

    Update pre.c

--- pre.c
+++ pre.c
@@ -2,13 +2,13 @@
 #include<string.h>
 int main()
 {
-	int n,l,p,i;
+	int n,l,p,k;
 	char s[100];
     scanf("%s",s);
 	scanf("%d",&n);
 	l=strlen(s);
 	p=l-n;
-	for(i=p;i<l;i++)
+	for(k=p;k<l;k++)
 	{
 		printf("%c",s[i]);
 	}
