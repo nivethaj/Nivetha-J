2f0351fd3a94ae064fc7184a250bfca10466906a
nivethaj <30828724+nivethaj@users.noreply.github.com>
2018-03-02 16:58:19 +0530

    Create append.c

---
+++ append.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main()
+{
+    char s[20],m,n='.';
+    int i=0;
+    printf("enter the string");
+    scanf("%s",&s);
+    m=strlen(s);
+   s[a]='.';
+   for(i=0;s[i]!='\0';i++)
+    {
+    printf("%c",s[i]);
+}
+
+}
