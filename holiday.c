9eedd6ec57b9474b353116ebeee4d5a3eacda223
nivethaj <30828724+nivethaj@users.noreply.github.com>
2018-03-09 16:19:39 +0530

    Create holiday.c

---
+++ holiday.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main()
+{
+    char s,i;
+    printf("\n enter a day:");
+    scanf("%s",&i);
+    if(i==0||i==6)
+    {
+    printf("holiday");
+    }
+    else
+    {
+      printf("\nNo");
+      }
+      return 0;
+}
