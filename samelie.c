fdbe255879743425c40044a73525afd2606321d0
nivethaj <30828724+nivethaj@users.noreply.github.com>
2018-03-20 16:31:29 +0530

    Create samelie.c

---
+++ samelie.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void main() 
+{
+	int a,b,c,n;
+	printf("\n Enter a number:");
+	scanf("%d%d%d",&a,&b,&c);
+	n=a,b,c;
+	if(n==1)
+	{
+	    printf("\n yes");
+	}
+	else
+	{
+	    printf("\n no");
+	}
+	return 0;
+}	
+	
